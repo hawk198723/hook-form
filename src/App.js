03919f4f7a623a0c70b6d61f1ba69995e6695326
Jason Wang <jason@Jasons-MacBook-Pro.local>
2022-04-20 16:58:54 -0700

    push new forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { firstName: "", lastName: "" },
+  });
+  const onSubmit = (data) => console.log(data);
+  const firstName = watch("firstName");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("firstName", {
+            required: "type field can not be empty",
+          })}
+          placeholder="First Name"
+        />
+        <p>{firstName}</p>
+        <p>{errors.firstName?.message}</p>
+        <input
+          {...register("lastName", {
+            required: "type field can not be empty",
+            minLength: { value: 4, message: "min length is 4" },
+          })}
+          placeholder="Last Name"
+        />
+        <p>{errors.lastName?.message}</p>
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
